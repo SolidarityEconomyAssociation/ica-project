fdb645b6fd8fb693fa2067d138eaac33c6c4c21a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-09-15 14:06:03 +0100

    config/popup.js - encapsulate address logic in function

--- config/popup.js
+++ config/popup.js
@@ -9,8 +9,48 @@ var getHoverText = function (initiative) {
 var prettyPhone = function (tel) {
   return tel.replace(/^(\d)(\d{4})\s*(\d{6})/, "$1$2 $3");
 };
+function getAddress(initiative, getTerm) {
+  // We want to add the whole address into a single para
+  // Not all orgs have an address
+  let address = "";
+  let street;
+  if (initiative.street) {
+    let streetArray = initiative.street.split(";");
+    for (let partial of streetArray) {
+      if (partial === initiative.name) continue;
+      if (street) street += "<br/>";
+      street = street ? (street += partial) : partial;
+    }
+    address += street;
+  }
+  if (initiative.locality) {
+    address += (address.length ? "<br/>" : "") + initiative.locality;
+  }
+  if (initiative.region) {
+    address += (address.length ? "<br/>" : "") + initiative.region;
+  }
+  if (initiative.postcode) {
+    address += (address.length ? "<br/>" : "") + initiative.postcode;
+  }
+  if (initiative.countryId) {
+    const countryName = getTerm('countryId');
+    address += (address.length ? "<br/>" : "") + (countryName || initiative.countryId);
+  }
+  if (initiative.nongeo == 1 || !initiative.lat || !initiative.lng) {
+    address += (address.length ? "<br/>" : "") + "<i>NO LOCATION AVAILABLE</i>";
+  }
+  if (address.length) {
+    address = '<p class="sea-initiative-address">' + address + "</p>";
+  }
+  return address;
+}
 
 var getPopup = function (initiative, sse_initiatives) {
+  function getTerm(propertyName) {
+    const vocabUri = sse_initiatives.getVocabUriForProperty(propertyName);
+    return sse_initiatives.getVocabTerm(vocabUri, initiative[propertyName]);
+  }
+  
   const values = sse_initiatives.getLocalisedVocabs();
   const labels = sse_initiatives.getFunctionalLabels();
   let orgStructures = values["os:"].terms;
@@ -24,34 +64,29 @@ var getPopup = function (initiative, sse_initiatives) {
   // Initiative's dotcoop domains. Note, not all have a website.
   let dotcoop_domains = "";
   if (initiative.www)
-      dotcoop_domains = `<a href="${initiative.www}" target="_blank" >${initiative.www}</a>`;
+    dotcoop_domains = `<a href="${initiative.www}" target="_blank" >${initiative.www}</a>`;
   
-  let address = "",
-      street,
-      locality,
-      postcode,
-      dotcoop =
-        initiative.dataset.includes("dotcoop"),
-      popupHTML =
-        '<div class="sea-initiative-details">' +
-        `<h2 class="sea-initiative-name">${initiative.name}</h2>` +
-        `${dotcoop_domains || ''}` +
-        `<h4 class="sea-initiative-org-structure">${values["os:"].title}: ${orgStructures[initiative.regorg]}</h4>` +
-        `<h4 class="sea-initiative-org-typology">${values["bmt:"].title}: ${membershipsVerbose[initiative.baseMembershipType]}</h4>` +
-        `<h4 class="sea-initiative-economic-activity">${values["aci:"].title}: ${activitiesVerbose[initiative.primaryActivity]}</h4>` +
-        '<h5 class="sea-initiative-secondary-activity">Secondary Activities: {initiative.secondary-activity}</h5>' +
-        `<p>${initiative.desc || ''}</p>` +
-        "</div>" +
-        '<div class="sea-initiative-contact">' +
-        `<h3>${labels.contact}</h3>` +
-        "{initiative.address}" +
-        "{initiative.tel}" +
-        '<div class="sea-initiative-links">' +
-        "{initiative.email}" +
-        "{initiative.facebook}" +
-        "{initiative.twitter}" +
-        "</div>" +
-        "</div>";
+  let dotcoop = initiative.dataset.includes("dotcoop");
+  let popupHTML =
+    '<div class="sea-initiative-details">' +
+    `<h2 class="sea-initiative-name">${initiative.name}</h2>` +
+    `${dotcoop_domains || ''}` +
+    `<h4 class="sea-initiative-org-structure">${values["os:"].title}: ${orgStructures[initiative.regorg]}</h4>` +
+    `<h4 class="sea-initiative-org-typology">${values["bmt:"].title}: ${membershipsVerbose[initiative.baseMembershipType]}</h4>` +
+    `<h4 class="sea-initiative-economic-activity">${values["aci:"].title}: ${activitiesVerbose[initiative.primaryActivity]}</h4>` +
+    '<h5 class="sea-initiative-secondary-activity">Secondary Activities: {initiative.secondary-activity}</h5>' +
+    `<p>${initiative.desc || ''}</p>` +
+    "</div>" +
+    '<div class="sea-initiative-contact">' +
+    `<h3>${labels.contact}</h3>` +
+    `${getAddress(initiative, getTerm)}` +
+    "{initiative.tel}" +
+    '<div class="sea-initiative-links">' +
+    "{initiative.email}" +
+    "{initiative.facebook}" +
+    "{initiative.twitter}" +
+    "</div>" +
+    "</div>";
   // TODO Add org type
   if (!initiative.qualifier && initiative.orgStructure && initiative.orgStructure.length > 0) {
     let repl = initiative.orgStructure.map(OS => orgStructures[OS]).join(", ");
@@ -123,39 +158,6 @@ var getPopup = function (initiative, sse_initiatives) {
       "Typology: {initiative.org-baseMembershipType}", "Others"
     )
   }
-
-  // We want to add the whole address into a single para
-  // Not all orgs have an address
-  if (initiative.street) {
-    let streetArray = initiative.street.split(";");
-    for (let partial of streetArray) {
-      if (partial === initiative.name) continue;
-      if (street) street += "<br/>";
-      street = street ? (street += partial) : partial;
-    }
-    address += street;
-  }
-  if (initiative.locality) {
-    address += (address.length ? "<br/>" : "") + initiative.locality;
-  }
-  if (initiative.region) {
-    address += (address.length ? "<br/>" : "") + initiative.region;
-  }
-  if (initiative.postcode) {
-    address += (address.length ? "<br/>" : "") + initiative.postcode;
-  }
-  if (initiative.countryId) {
-    const vocabUri = sse_initiatives.getVocabUriForProperty('countryId');
-    const countryName = sse_initiatives.getVocabTerm(vocabUri, initiative.countryId);
-    address += (address.length ? "<br/>" : "") + (countryName || initiative.countryId);
-  }
-  if (initiative.nongeo == 1 || !initiative.lat || !initiative.lng) {
-    address += (address.length ? "<br/>" : "") + "<i>NO LOCATION AVAILABLE</i>";
-  }
-  if (address.length) {
-    address = '<p class="sea-initiative-address">' + address + "</p>";
-  }
-  popupHTML = popupHTML.replace("{initiative.address}", address);
 
   // Not all orgs have an email
   if (initiative.email) {
